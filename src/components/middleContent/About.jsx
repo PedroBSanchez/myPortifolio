4babb23e1a318e91ae09aa23f0bea39db08f61f1
PedroBSanchez <pedro_b47@hotmail.com>
2023-01-16 22:42:33 -0300

    feature[

--- src/components/middleContent/About.jsx
+++ src/components/middleContent/About.jsx
@@ -6,13 +6,17 @@ import "./About.css";
 import "../../App.css";
 
 const About = () => {
+  const handleClick = (link) => {
+    if (link) window.open(link);
+  };
+
   return (
     <>
       <div className="col-md-6 p-5">
         <div className="card-border-about">
           <div className="row">
-            <div className="col-md-6 text-center p-1">
-              <img className="about-image img-fluid" src={image} alt={"Me"} />
+            <div className="col-md-6 text-center pt-4">
+              <img className="about-image" src={image} alt={"Me"} />
             </div>
             <div className="col-md-6">
               <div className="row m-4">
@@ -32,6 +36,46 @@ const About = () => {
                   </li>
                 </ul>
               </div>
+            </div>
+          </div>
+          <div className="row text-center justify-content-center">
+            <div className="col-2 p-1">
+              <h4>
+                <span
+                  class="badge bg-primary card-badge"
+                  onClick={() => {
+                    handleClick("https://github.com/PedroBSanchez");
+                  }}
+                >
+                  GitHub
+                </span>
+              </h4>
+            </div>
+            <div className="col-2 p-1">
+              <h4>
+                <span
+                  class="badge bg-primary card-badge"
+                  onClick={() => {
+                    handleClick(
+                      "https://www.linkedin.com/in/pedro-sanchez-b884831b2/"
+                    );
+                  }}
+                >
+                  Linkedin
+                </span>
+              </h4>
+            </div>
+            <div className="col-2 p-1">
+              <h4>
+                <span
+                  class="badge bg-primary card-badge"
+                  onClick={() => {
+                    handleClick("https://utapy.link/pedrobsanchez");
+                  }}
+                >
+                  Email
+                </span>
+              </h4>
             </div>
           </div>
         </div>
